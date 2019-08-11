eac4530cd0c8044dee809fe1233366f682b680f1
jsierra30 <53976010+jsierra30@users.noreply.github.com>
2019-08-11 11:30:35 -0500

    Primera version1
    Enviar rama 1

---
+++ ejercicio1/CursoReactJs/cursoreactjs/src/components/Hider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mapa from './Mapa';
+class Hider extends React.Component{
+  render(){
+    return (<div id='mapa'>
+      <Mapa />
+    </div>                    
+    )
+  }
+
+}
+
+export default Hider
---
+++ ejercicio1/CursoReactJs/cursoreactjs/src/components/Mapa.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Mapa extends React.Component{
+  render(){
+    return (<ul>
+        <li>
+            Lista 1
+        </li>
+        <li>
+            Lista 2
+        </li>
+    </ul>)
+  }
+
+}
+export default Mapa
